40c4eea62ed3c7ae909c80e40179fcdcfcf26861
Innocent <nsabimanaii@hendrix.edu>
2020-04-30 14:17:24 -0500

    added parsers

---
+++ AParser.hs
@@ -0,0 +1,107 @@
+-- {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE InstanceSigs         #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module AParser (Parser, runParser, satisfy, char, posInt) where
+
+import           Data.Char
+import           Control.Applicative
+
+-- A parser for a value of type a is a function which takes a String
+-- represnting the input to be parsed, and succeeds or fails; if it
+-- succeeds, it returns the parsed value along with the remainder of
+-- the input.
+newtype Parser a where
+  P :: (String -> Maybe (a, String)) -> Parser a
+
+runParser :: Parser a -> String -> Maybe (a, String)
+runParser (P f) = f
+
+-- For example, 'satisfy' takes a predicate on Char, and constructs a
+-- parser which succeeds only if it sees a Char that satisfies the
+-- predicate (which it then returns).  If it encounters a Char that
+-- does not satisfy the predicate (or an empty input), it fails.
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = P f
+  where
+    f [] = Nothing    -- fail on an empty input
+    f (x:xs)          -- check if x satisfies the predicate
+                        -- if so, return x along with the remainder
+                        -- of the input (that is, xs)
+        | p x       = Just (x, xs)
+        | otherwise = Nothing  -- otherwise, fail
+
+-- Using satisfy, we can define the parser 'char c' which expects to
+-- see exactly the character c, and fails otherwise.
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+{- For example:
+
+*Parser> runParser (satisfy isUpper) "ABC"
+Just ('A',"BC")
+*Parser> runParser (satisfy isUpper) "abc"
+Nothing
+*Parser> runParser (char 'x') "xyz"
+Just ('x',"yz")
+
+-}
+
+-- For convenience, I've also provided a parser for positive
+-- integers.
+posInt :: Parser Integer
+posInt = P f
+  where
+    f xs
+      | null ns   = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b 
+  fmap g (P f) = P (\s -> case (f s) of
+                         Nothing -> Nothing
+                         Just(a, s) -> Just((g a), s))
+
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure a = P (\s -> Just(a, s)) 
+
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  P g <*> P f = P
+      (\s -> case g s of
+        Nothing -> Nothing
+        Just(fb, s') ->  case (f s') of 
+          Nothing -> Nothing
+          Just (a, s'') -> Just(fb a, s'')
+      )
+
+
+instance Alternative Parser where
+    empty :: Parser a
+    empty = P(\_ -> Nothing)
+
+    (<|>) :: Parser a -> Parser a -> Parser a
+    (P f1) <|> (P f2) =  P 
+          (\s -> case (f1 s) of 
+            Nothing -> case (f2 s) of 
+              Nothing -> Nothing
+              Just(a'', s'') -> Just(a'', s'')
+            Just(a', s') -> Just(a', s')
+          )
+
+
+posChar :: Parser Char
+posChar = P f
+  where
+    f xs
+      | null ns   = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isUpper xs
+
+
+
+
---
+++ Assembler.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE InstanceSigs         #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+import AParser
+
+import                      Data.Char
+import                      Control.Applicative
+import                      Data.String
+
+-- // This file is part of www.nand2tetris.org
+-- // and the book "The Elements of Computing Systems"
+-- // by Nisan and Schocken, MIT Press.
+-- // File name: projects/06/add/Add.asm
+
+-- // Computes R0 = 2 + 3  (R0 refers to RAM[0])
+
+-- @2
+-- D=A
+-- @3
+-- D=D+A
+-- @0
+-- M=D
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = fmap(\s ss -> s: ss) p <*> zeroOrMore p 
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = (oneOrMore p) <|> pure []
+
+aInstParser_ :: Parser ()
+aInstParser_ = fmap (\_ -> ()) (char '@')
+
+stringParser :: Parser [Char]
+stringParser = fmap oneOrMore satisfy(isAlpha)
+
+cInstParser_ :: Parser ([Char],[Char])
+cInstParser_ = fmap(\s1 -> \s2 -> (s1,s2)) (stringParser) <*> (((char '=') <|> (char ';')) *> (stringParser)) 
+
+
+-- To tese for cInstParser
+-- runParser aInstParser_  "@ABC"
+
+-- To test for aInstparser
+-- runParser cInstParser_  "AB=C"
+
