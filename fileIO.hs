fb0c0c0a73170cc6ba93219ce145d91876c62567
theDrunkenManatee <samorege@hendrix.edu>
2020-04-30 16:48:09 -0500

    IO start!

---
+++ fileIO.hs
@@ -0,0 +1,12 @@
+import System.IO
+
+handle = readFile "testFile.txt"
+testVal = "Hello!"
+
+
+parseFile :: String -> [String]
+parseFile path
+  | x == '\n' = [xs]
+  | otherwise = [x]
+  where
+    [x:xs] <- readFile path
