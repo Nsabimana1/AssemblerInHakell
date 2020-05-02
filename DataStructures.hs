5506a109840fb9ffbe12a8567d7865a055838494
Innocent <nsabimanaii@hendrix.edu>
2020-05-01 20:42:39 -0500

    Auto stash before merge of "master" and "origin/master"

--- DataStructures.hs
+++ DataStructures.hs
@@ -116,7 +116,10 @@ aInstParser :: Parser Instruction
 aInstParser = removesymbol *> ((fmap(\s -> A_Inst s) (posInt)) <|> (fmap(\s -> A_Inst (parsemaybehelper (M.lookup s symbolTable))) (stringParser)))
 
 cInstParser :: Parser ([Char],[Char])
-cInstParser = fmap(\s1 -> \s2 -> (s1,s2)) (stringParser) <*> (((char '=') <|> (char ';')) *> (stringParser)) 
+cInstParser = fmap(\s1 -> \s2 ->(s1,s2)) (stringParser) <*> (((char '=') <|> (char ';')) *> (stringParser)) 
+
+-- destParser :: Parser Dest
+-- destParser = Zero
 
 --Now as a Map!
 symbolTable = M.fromList([("KBD", 16384), ("SP", 0), ("LCL", 1), ("ARG", 2), ("THIS", 3), ("THAT", 4), ("R0", 0), ("R1", 1), ("R2", 2), ("R3", 3), ("R4", 4), ("R5", 5), ("R6", 6), ("R7",7), ("R8", 8), ("R9", 9), ("R10", 10), ("R11", 11), ("R12", 12), ("R13", 13), ("R14", 14), ("R15", 15), ("currentSymbol", 16), ("curLine", 0)]) :: (M.Map String Integer)
